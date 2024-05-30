f2f4dcad096415f9256f23536cd8b5669ceede0a
mrrobot <mahaon_1990@mail.ru>
2024-05-30 19:14:39 +0300

    дроп докер порты

--- assistant_api/src/core/logger.py
+++ assistant_api/src/core/logger.py
@@ -1,14 +1,15 @@
 import logging
+import sys
+
 import logging_loki
 
-handler = logging_loki.LokiHandler(
+loki = logging_loki.LokiHandler(
     url='http://loki:3100/loki/api/v1/push',
     tags={"application": "assistant_api"},
 )
+sh = logging.StreamHandler(sys.stdout)
 
-logger = logging.getLogger('assistant')
-logger.addHandler(handler)
-logger.error(
-    "Something happend"
-)
-
+logger = logging.getLogger("assistant_api")
+logger.addHandler(loki)
+logger.addHandler(sh)
+logger.setLevel(logging.INFO)
